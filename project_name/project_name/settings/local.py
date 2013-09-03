6de1e753df6649aa7a46447e4a11ada621c228e7
proft <proftua@gmail.com>
2013-09-03 19:21:14 +0300

    disable django debug

--- project_name/project_name/settings/local.py
+++ project_name/project_name/settings/local.py
@@ -24,7 +24,7 @@ INSTALLED_APPS += (
 INTERNAL_IPS = ('127.0.0.1',)
 
 MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 DEBUG_TOOLBAR_CONFIG = {
