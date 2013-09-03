31e78ae046bd5ac3dd653a080cdf7802c7dd64de
proft <proftua@gmail.com>
2013-09-03 17:57:54 +0300

    update settings

--- project_name/project_name/settings/local.py
+++ project_name/project_name/settings/local.py
@@ -7,7 +7,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '',
         'USER': '',
         'PASSWORD': '',
--- project_name/project_name/settings/test.py
+++ project_name/project_name/settings/test.py
@@ -1,11 +1,10 @@
 from base import *
 
-########## TEST SETTINGS
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
 TEST_DISCOVER_ROOT = SITE_ROOT
 TEST_DISCOVER_PATTERN = "test_*.py"
-########## IN-MEMORY TEST DATABASE
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
