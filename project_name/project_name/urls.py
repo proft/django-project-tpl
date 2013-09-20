cb413e8b87bc9bf2674ad6a316c407b75e8960ff
proft <proftua@gmail.com>
2013-09-20 19:02:51 +0300

    url for media

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,4 +12,11 @@ urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) 
+
+if settings.DEBUG:
+    urlpatterns += url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+            'show_indexes': True
+    })
+
